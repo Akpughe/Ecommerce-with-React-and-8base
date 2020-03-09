a51663edf6029711142c77faa097d3abbfd0c906
David <davidakpughe2@gmail.com>
2020-03-09 13:37:17 +0100

    fixed the permission issue

--- src/components/product-list/index.js
+++ src/components/product-list/index.js
@@ -2,19 +2,19 @@ import gql from "graphql-tag";
 import { graphql } from "react-apollo";
 
 const PRODUCTS_QUERY = gql`
-  query {
-    productsList {
-      items {
-        id
-        createdAt
-        name
-        price
-        description
-        image {
-          downloadUrl
-        }
+{
+  productsList {
+    items {
+      id
+      createdAt
+      name
+      price
+      description
+      image {
+        downloadUrl
       }
     }
   }
+}
 `;
 export default PRODUCTS_QUERY;
